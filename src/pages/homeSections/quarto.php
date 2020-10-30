8cb173eb4e887a4360649c52adc6066b37f969f5
tiago de lara ribeiro <laratecsys@gmail.com>
2020-10-29 22:28:18 -0300

    ma

---
+++ src/pages/homeSections/quarto.php
@@ -0,0 +1,11 @@
+
+<link rel="stylesheet" type="text/css" href="./styles/formHome.css">
+<div class="third-section-container">
+
+    <h1>Funcionalidades</h1>
+    <form class='container-card'>
+        <input tpye="text"  placeholder="Nome">
+        <input type="email" placeholder="Email">
+        <butto type="submit"></button>
+    </form>
+</div>
