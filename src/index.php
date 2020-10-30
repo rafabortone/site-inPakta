d72fad87431bb66377522c832b76d66ee6c2bc64
rafael <rafael@bluefoot.com.br>
2020-10-29 22:25:13 -0300

    aplica headrrom

--- src/index.php
+++ src/index.php
@@ -7,6 +7,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../styles/global.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script type="text/javascript" src="scripts/headroom.js"></script>
 
   <title>inPakta</title>
 </head>
@@ -19,6 +21,6 @@
     include('pages/home.php');
   ?>
 
-
+<script type="text/javascript" src="scripts/global.js"></script>
 </body>
 </html>
